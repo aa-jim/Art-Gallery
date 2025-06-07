e784b1308b62e53203b4320f2031af8116feca75
aa-jim <abdullahaljim2@gmail.com>
2025-06-07 19:59:59 +0600

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,17 @@ import React, { useState } from "react";
 import "./Navbar.css";
 
 export default function Navbar() {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <nav className="navbar-container">
       <div className="logo">A A JIM</div>
-
-      {/* Hamburger Icon */}
-      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+      <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
         ☰
       </div>
-
-      <ul className={`nav-links ${menuOpen ? "show" : ""}`}>
-        <li><a href="#home">HOME</a></li>
-        <li><a href="#about">ABOUT ME</a></li>
+      <ul className={`nav-links ${isOpen ? "show" : ""}`}>
+        <li><a href="#home" onClick={() => setIsOpen(false)}>HOME</a></li>
+        <li><a href="#about" onClick={() => setIsOpen(false)}>ABOUT ME</a></li>
       </ul>
     </nav>
   );
